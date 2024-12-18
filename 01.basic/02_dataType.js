13f52a49f666b587962750562413d1f0bb6070e6
Harsh Tiwari <95462159+504harsh@users.noreply.github.com>
2024-12-18 06:18:11 +0000

    datatype

---
+++ 01.basic/02_dataType.js
@@ -0,0 +1,32 @@
+"use strict"; //treat all js code as newer version
+
+// alert("hello"); // we are using nodejs, not browser
+
+console.log(3 
+    + 
+    3) //code readability should be high
+
+console.log("Harsh")
+
+
+let name = "harsh"
+let age = 21
+let isLoggedIn = false
+let state;
+
+
+//number => 1 to power 53
+//bigint 
+//string => ""
+//boolean => true or false
+//null => standalone value
+//undefined
+//symbol => unique define krne ke liya
+
+//object
+
+console.log(typeof name);
+console.log(typeof null); //object
+console.log(typeof undefined); //undefined
+
+
